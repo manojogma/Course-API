72ccc00013d236ba9d528971e0ec8bc8e2ce2284
navinOgma <navin@ogmaconceptions.com>
2024-10-19 16:16:03 +0530

    callback replace with promise

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 console.log('Before');
- getUser(1, getRepositories);
+
+const p = getUser(1);
+p.then(user => getRepositories(user.gitHubUsername))
+ .then(repos => getCommits(repos[0]))
+ .then(commits => console.log('Commits', commits))
+ .catch(err => console.log('Error', err.message));
+
 console.log('After');
 
-function displayCommits (commits) {
-  console.log(commits);
+// function displayCommits (commits) {
+//   console.log(commits);
+// }
+
+// function getCommits (repos) {
+//   getCommits(repo, displayCommits);
+// }
+
+// function getRepositories (user) {
+//   getRepositories(user.gitHubUsername, getCommits);
+// }
+
+function getUser(id) {
+  return new Promise ( (resolve, reject) => {
+    setTimeout(() => {
+      console.log('reading from a database...');
+      resolve( { id: id, gitHubUsername: 'manoj'});
+    }, 2000);
+  });
 }
 
-function getCommits (repos) {
-  getCommits(repo, displayCommits);
+function getRepositories(username) {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      console.log('reading from a github...');
+      resolve ( ['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  })
 }
 
-function getRepositories (user) {
-  getRepositories(user.gitHubUsername, getCommits);
+function getCommits(repo) {
+  return new Promise ((resolve, reject) => {
+    setTimeout ( () => {
+      console.log('Calling from Github Api...');
+      resolve(['commits']);
+    }, 2000);
+  });
 }
-
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('reading from a database...');
-    callback( { id: id, gitHubUsername: 'manoj'});
-  }, 2000);
-}
-
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('reading from a github...');
-    callback ( ['repo1', 'repo2', 'repo3']);
-  }, 2000);
-}
-
-
-
-
 
